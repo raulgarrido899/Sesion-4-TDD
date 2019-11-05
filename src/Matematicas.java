cee08fb481395c3d24804432247712c298d58632
usuario <usuario@pc21-084>
2019-11-05 17:43:20 +0100

    Tercera version con los metodos funcionando.

--- src/Matematicas.java
+++ src/Matematicas.java
@@ -1,10 +1,10 @@
 public class Matematicas {
     
     public static int Suma(int a, int b){
-        return 5;
+        return a+b;
     }
     
     public static int Resta(int a, int b){
-        return 5;
+        return a-b;
     }
 }
