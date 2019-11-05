dc42d609f928b25adfd731a7c67c395f3f27ce45
usuario <usuario@pc21-084>
2019-11-05 17:37:47 +0100

    Primera version sin clases.

---
+++ test/TestMatematicas.java
@@ -0,0 +1,29 @@
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class TestMatematicas {
+    
+    public TestMatematicas() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Test
+    public void testSuma(){
+        assertEquals(5, Matematicas.Suma(2,3));
+    }
+    
+    @Test
+    public void testResta(){
+        assertEquals(5, Matematicas.Resta(7,2));
+    }
+}
